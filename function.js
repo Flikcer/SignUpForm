6ec82e110d34a462d1add88b25c4168c3337d961
Anthony <129002769+Flikcer@users.noreply.github.com>
2023-04-10 21:23:20 -0400

    Fixed functionality on JS erroring and style

--- function.js
+++ function.js
@@ -2,18 +2,19 @@
 const form = document.querySelector("form");
 
 // Get the input elements
+const usernameInput = document.querySelector("#username");
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
 const confirmPasswordInput = document.querySelector("#confirm-password");
 
 // Get the error elements
+const usernameError = document.querySelector("#username-error");
 const nameError = document.querySelector("#name-error");
 const emailError = document.querySelector("#email-error");
 const passwordError = document.querySelector("#password-error");
 const confirmPasswordError = document.querySelector("#confirm-password-error");
 
-// Define the checkValidity function
 function checkValidity(input, error) {
   if (input.validity.valueMissing && input === document.activeElement) {
     error.textContent = "Please fill out this field.";
@@ -23,6 +24,12 @@ function checkValidity(input, error) {
 
   if (input.validity.typeMismatch && input === document.activeElement) {
     error.textContent = "Please enter a valid email address.";
+    input.classList.add("error");
+    return false;
+  }
+
+  if (input.validity.tooShort && input === document.activeElement) {
+    error.textContent = `Username must be at least ${input.minLength} characters long.`;
     input.classList.add("error");
     return false;
   }
@@ -49,6 +56,10 @@ function checkPasswords() {
 }
 
 // Add event listeners to the input fields
+usernameInput.addEventListener("blur", () => {
+  checkValidity(usernameInput, usernameError);
+});
+
 nameInput.addEventListener("blur", () => {
   checkValidity(nameInput, nameError);
 });
@@ -63,18 +74,23 @@ confirmPasswordInput.addEventListener("blur", checkPasswords);
 // Add an event listener to the form's submit event
 form.addEventListener("submit", (event) => {
   // Check the validity of the input fields
+  const isUsernameValid = checkValidity(usernameInput, usernameError);
   const isNameValid = checkValidity(nameInput, nameError);
   const isEmailValid = checkValidity(emailInput, emailError);
   const isPasswordsValid =
     passwordInput.checkValidity() && confirmPasswordInput.checkValidity();
 
   // If any input field is invalid, prevent submission
-  if (!isNameValid || !isEmailValid || !isPasswordsValid) {
+  if (!isUsernameValid || !isNameValid || !isEmailValid || !isPasswordsValid) {
     event.preventDefault();
   }
 });
 
 // Add event listeners to the input fields to remove error class on input
+usernameInput.addEventListener("input", () => {
+  checkValidity(usernameInput, usernameError);
+});
+
 nameInput.addEventListener("input", () => {
   checkValidity(nameInput, nameError);
 });
